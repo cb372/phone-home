1023cab7772669bf43bf3a40586c4f000ffcc5b5
Chris Birchall <chris.birchall@gmail.com>
2013-05-19 18:22:55 +0900

    Javscript cleanup

--- client/phone-home.js
+++ client/phone-home.js
@@ -1,94 +1,86 @@
 
-// namespace
-var PhoneHome = {};
-
-PhoneHome.PH = (function() {
-    // private
-    var _options = { 
-        app: "(unspecified)",
-        password: "not so secret",
-        swallowErrors: true
-    };
-
-    var _serverUrl = "http://example.com/";
-
-    var _ensureTrailingSlash = function(url) {
-        if (url[url.length - 1] == '/')
-            return url;
-        else
-            return url + '/';
-    };
-
-    var _buildCommonPayload = function() {
-        return {
-            app: _options.app,
-            url: document.URL,
-            userAgent: navigator.userAgent,
-            customFields: _options.customFields // may or may not be defined
+if (typeof PhoneHome !== 'object') {
+    var PhoneHome = (function() {
+        // private
+        var _options = { 
+            app: "(unspecified)",
+            password: "not so secret",
+            swallowErrors: true
         };
-    }
 
-    var _sendXHR = function(url, payload) {
-        xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("X-PhoneHome-Auth", _options.password);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(payload));
-    };
+        var _serverUrl = "http://example.com/";
 
-    var _sendError = function(e) {
-        try {
-            var payload = _buildCommonPayload();
-            payload.error = {
-                name: e.name,
-                message: e.message,
-                file: e.fileName, // FF only
-                line: e.lineNumber, // FF only
+        var _ensureTrailingSlash = function(url) {
+            if (url[url.length - 1] == '/')
+                return url;
+            else
+                return url + '/';
+        };
+
+        var _buildCommonPayload = function() {
+            return {
+                app: _options.app,
+                url: document.URL,
+                userAgent: navigator.userAgent,
+                customFields: _options.customFields // may or may not be defined
             };
-
-            alert(JSON.stringify(payload));
-
-            _sendXHR(_serverUrl + "errors", payload)
-        } catch(e) { }
-    };
-
-    var _sendMessage = function(message) {
-        try {
-            var payload = _buildCommonPayload();
-            payload.message = message;
-
-            alert(JSON.stringify(payload));
-
-            _sendXHR(_serverUrl + "messages", payload)
-        } catch(e) { }
-    };
-
-    return {
-        // public
-
-        init: function(serverUrl, options) {
-            _serverUrl = _ensureTrailingSlash(serverUrl);
-            for (var property in options) {
-                if (options.hasOwnProperty(property)) {
-                    _options[property] = options[property];
-                }
-            }
-        },
-
-        wrap: function(f) {
-            try {
-                f();
-            } catch (e) {
-                _sendError(e);
-                if (!_options.swallowErrors) {
-                    throw e;
-                }
-            }      
-        },
-
-        sendMessage: function(message) {
-            _sendMessage(message);
         }
-    }
-})();
 
+        var _sendXHR = function(url, payload) {
+            xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("X-PhoneHome-Auth", _options.password);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(payload));
+        };
+
+        var _sendError = function(e) {
+            try {
+                var payload = _buildCommonPayload();
+                payload.error = {
+                    name: e.name,
+                    message: e.message,
+                    file: e.fileName, // FF only
+                    line: e.lineNumber, // FF only
+                };
+                _sendXHR(_serverUrl + "errors", payload)
+            } catch(e) { }
+        };
+
+        var _sendMessage = function(message) {
+            try {
+                var payload = _buildCommonPayload();
+                payload.message = message;
+                _sendXHR(_serverUrl + "messages", payload)
+            } catch(e) { }
+        };
+
+        return {
+            // public
+
+            init: function(serverUrl, options) {
+                _serverUrl = _ensureTrailingSlash(serverUrl);
+                for (var property in options) {
+                    if (options.hasOwnProperty(property)) {
+                        _options[property] = options[property];
+                    }
+                }
+            },
+
+            wrap: function(f) {
+                try {
+                    f();
+                } catch (e) {
+                    _sendError(e);
+                    if (!_options.swallowErrors) {
+                        throw e;
+                    }
+                }      
+            },
+
+            sendMessage: function(message) {
+                _sendMessage(message);
+            }
+        }
+    })();
+}
