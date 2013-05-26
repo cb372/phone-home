b98936c3ea60df8c59185433a4a70e46e1145c09
Chris Birchall <chris.birchall@gmail.com>
2013-05-26 20:32:15 +0900

    Add bootstrap, make templates pretty

--- server/src/main/scala/com/github/cb372/phonehome/StaticResourcesController.scala
+++ server/src/main/scala/com/github/cb372/phonehome/StaticResourcesController.scala
@@ -1,11 +1,5 @@
 package com.github.cb372.phonehome
 
-import org.scalatra._
-
-class StaticResourcesController extends ScalatraServlet {
-
-  notFound {
-    serveStaticResource() getOrElse resourceNotFound()
-  }
+class StaticResourcesController extends PhonehomeServerStack {
 
 }
